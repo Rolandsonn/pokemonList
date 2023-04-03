e7c0df2d97c7657903d0461c127119888587f246
Ruslan <stamkulovruslan72@gmail.com>
2023-04-03 15:53:17 +0600

    pokemon list is rendered

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useEffect, useState } from "react";
+import "./App.css";
+import fecthPokemons from "./api/fecthPokemons";
+import PokemonList from "./components/PokemonList/PokemonList";
+import sun from "./assets/img/sun.png";
+import moon from "./assets/img/moon.png";
 function App() {
+  const [theme, setTheme] = useState("light");
+  const [pokemonList, setPokemonList] = useState([]);
+
+  const toggleTheme = () => {
+    const newTheme = theme === "dark" ? "light" : "dark";
+    setTheme(newTheme);
+  };
+
+  useEffect(() => {
+    fecthPokemons().then((pokeList) => setPokemonList(pokeList.results));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`app ${theme}`}>
+      <button className="lightBtn" onClick={toggleTheme}>
+        {theme === "dark" ? (
+          <img className="img" src={moon} alt="moon" />
+        ) : (
+          <img className="img" src={sun} alt="" />
+        )}
+      </button>
+      <PokemonList pokemonList={pokemonList} />
     </div>
   );
 }
---
+++ src/components/PokemonCard/PokemonCard.jsx
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from "react";
+import styles from "./PokemonCard.module.css";
+import axios from "axios";
+
+const PokemonCard = ({ name, url }) => {
+  const [imgPokemon, setImagePokemon] = useState();
+  const fetchPokemon = async () => {
+    try {
+      const { data } = await axios(url);
+      const img = data.sprites.other.dream_world.front_default;
+      setImagePokemon(img);
+    } catch (error) {
+      console.log(error + "FetchingPokemon failed PokemonCard");
+    }
+  };
+
+  useEffect(() => {
+    if (url) {
+      fetchPokemon();
+    }
+  }, [url]);
+
+  return (
+    <li className={styles.card}>
+      <img className={styles.img} src={imgPokemon} alt="pokemon" />
+      <p className={styles.text}>{name}</p>
+    </li>
+  );
+};
+
+export default PokemonCard;
